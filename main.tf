fe0cf1b1df81fa803c59c64aa63eceead4dbe2b1
Yu-kiYamano <yuki.yamano@topgate.co.jp>
2020-07-21 11:02:18 +0900

    add

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "google" {
-  #   credentials = "${file{"/Users/yamanoyuuki/Documents/terraform/sandbox-yukiyamano-3727d72f34f6.json"}}_"
   project = "sandbox-yukiyamano"
   region  = "asia-northeast1"
 }
 
 resource "google_compute_instance" "terraformstydy1" {
-  count = 2
   name         = "terraformstudy1"
   machine_type = "f1-micro"
   zone         = "asia-northeast1-b"
@@ -17,6 +15,10 @@ resource "google_compute_instance" "terraformstydy1" {
       type  = "pd-standard"
     }
   }
+  # labels = {
+  #     test = "terraformstudy1"
+  # }
+
   network_interface {
     network = "default"
   }
@@ -35,6 +37,70 @@ resource "google_compute_instance" "terraformstydy2" {
       type  = "pd-standard"
     }
   }
+
+  # labels = {
+  #     test = "terraformstudy2"
+  # }
+  network_interface {
+    network = "default"
+  }
+  
+}
+resource "google_compute_instance" "terraformstydy3" {
+  name         = "terraformstudy3"
+  machine_type = "f1-micro"
+  zone         = "asia-northeast1-b"
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      size  = 10
+      type  = "pd-standard"
+    }
+  }
+  # labels = {
+  #     test = "terraformstudy3"
+  # }
+  network_interface {
+    network = "default"
+  }
+  
+}
+resource "google_compute_instance" "terraformstydy4" {
+  name         = "terraformstudy4"
+  machine_type = "f1-micro"
+  zone         = "asia-northeast1-b"
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      size  = 10
+      type  = "pd-standard"
+    }
+  }
+  # labels = {
+  #     test = "terraformstudy4"
+  # }
+  network_interface {
+    network = "default"
+  }
+  
+}
+resource "google_compute_instance" "terraformstydy5" {
+  name         = "terraformstudy5"
+  machine_type = "f1-micro"
+  zone         = "asia-northeast1-b"
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      size  = 10
+      type  = "pd-standard"
+    }
+  }
+  # labels = {
+  #     test = "terraformstudy5"
+  # }
   network_interface {
     network = "default"
   }
